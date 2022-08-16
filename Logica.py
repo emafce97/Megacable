3f8426a8af52d66e9e5f1826ee4ec4db353f51ad
Ema <emafaillace2016@outlook.com>
2022-08-16 20:48:50 -0300

    Se creo la clase Logica y se implemento el metodo agregarCliente

---
+++ Logica.py
@@ -0,0 +1,16 @@
+class Logica:
+
+    @classmethod
+    def __lanzarExcepcion(cls,e):
+        print(f"Ocurrio la excepcion: {e}")
+
+    @classmethod
+    def agregarCliente(self):
+        datos_cliente = (input("Ingrese los datos separados por coma: "),)
+        try:
+            with Cursor() as cursor:
+                sentencia = "INSERT INTO clientes(nombre,apellido,dni) VALUES(%s,%s,%s)"
+                cursor.execute(sentencia,datos_cliente)
+                print("Se añadio el cliente")
+        except Exception as e:
+            cls.__lanzarExcepcion(e)
